e7a3dcf9062677011a6d5931b7f802561279f74c
grapswiz <ineeza@gmail.com>
2018-01-29 12:25:27 +0900

    Go のテストを追加

---
+++ pkg/setting/setting_test.go
@@ -0,0 +1,27 @@
+package setting
+
+import "testing"
+
+func TestPut(t *testing.T) {
+	i := Item{
+		Name:	"b",
+	}
+	s := Setting{
+		Items: []Item{
+			{Name:	"a", Value: "a"},
+		},
+	}
+	if len(s.Items) != 1 {
+		t.Errorf("前提条件ミス: %v", len(s.Items))
+	}
+	Put(&s, i)
+	if len(s.Items) != 2 {
+		t.Errorf("nameが異なる場合は挿入されないといけない: %v", len(s.Items))
+	}
+	i.Name = "a"
+	i.Value = "overwrite"
+	Put(&s, i)
+	if s.Items[0].Name != "a" || s.Items[0].Value != "overwrite" {
+		t.Errorf("nameが同一の場合はitemの値で上書きされないといけない")
+	}
+}
